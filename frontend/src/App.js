c0b62adf8144904956068bc6a710851edc8210f4
Guilherme sa <guilhermedesaster@gmail.com>
2018-05-05 13:06:33 -0300

    Testing Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Header } from 'react-native-elements';
+import Logo from './components/Logo';
+
+/*
+  using react-native-elements and react-native-vector-icons
+  https://github.com/react-native-training/react-native-elements
+  https://github.com/oblador/react-native-vector-icons
+*/
 
 class App extends Component {
   render() {
     return (
-      <Text>Hi</Text>
+      <View>
+        <Header
+          rightComponent={<Logo />}
+          leftComponent={{ icon: 'home', color: '#fff', size: 30 }}
+          centerComponent={<Text style={{ color: '#fff', margin: 0 }}>Avaliador: Vitor</Text>}
+        />
+        <Text>Some text here</Text>
+      </View>
     );
   }
 }
