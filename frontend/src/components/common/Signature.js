6d43074411f40db5b1258ce10765507bf8c9e1f9
Guilherme sa <guilhermedesaster@gmail.com>
2018-05-25 20:30:09 -0300

    refactoring signature component

--- frontend/src/components/common/Signature.js
+++ frontend/src/components/common/Signature.js
@@ -3,6 +3,10 @@ import { View, StyleSheet, TouchableHighlight, Text } from 'react-native';
 import SignatureCapture from 'react-native-signature-capture';
 
 class Signature extends Component {
+  constructor(props){
+    super(props);    
+  }
+  
   render() {
     return (
       <View style={styles.container}>
@@ -10,22 +14,10 @@ class Signature extends Component {
           <SignatureCapture
               style={[styles.signature]}
               ref="sign"
-              onSaveEvent={ (result) => { this.props.onSave(result) } }
+              onSaveEvent={this._onSaveEvent.bind(this)}
               onDragEvent={this._onDragEvent}
               showNativeButtons={false}
             />
-
-          <View style={{ flex: 1, flexDirection: "row" }}>
-            <TouchableHighlight style={styles.buttonStyle}
-                onPress={() => { this.saveSign() } } >
-                <Text>Salvar</Text>
-            </TouchableHighlight>
-
-            <TouchableHighlight style={styles.buttonStyle}
-                onPress={() => { this.resetSign() } } >
-                <Text>Limpar</Text>
-            </TouchableHighlight>
-          </View>
         </View>
       </View>
     )
@@ -40,12 +32,9 @@ class Signature extends Component {
   }
 
   _onSaveEvent(result) {
-    // TODO
-
-    //result.encoded - for the base64 encoded png
-    //result.pathName - for the file path name
-    console.log(result.encoded);
+    this.props.onSave && this.props.onSave(result);
   }
+
   _onDragEvent() {     
     // TODO if necessary
 
@@ -59,11 +48,6 @@ const styles = StyleSheet.create({
   },
   signature: {
       height: 200,
-  },
-  buttonStyle: {
-    flex: 1, justifyContent: "center", alignItems: "center", height: 50,
-    backgroundColor: "#eeeeee",
-    margin: 10
   }
 });
 
--- frontend/src/components/exams/TwelveMinutesRun.js
+++ frontend/src/components/exams/TwelveMinutesRun.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, Button } from 'react-native';
 import { Signature } from '../common';
 
 class TwelveMinutesRun extends Component {
 
-  save(result){
-    console.log(result.encoded)
+  saveImage() {
+    this.refs.signature.saveSign();
+  }
+
+  resetImage(){
+    this.refs.signature.resetSign();
+  }
+
+  onSave(result){
+    console.log("Base64 encoded image: " + result.encoded);
   }
 
   render() {
     return (
-      <View style={{width: 600}}>
-        <Signature onSave={ this.save.bind(this) } />
+      <View style={{flex: 1}}>
+        <View style={{width: 600}}>
+          <Signature ref='signature' onSave={this.onSave.bind(this)} />
+        </View>
+        <View style={{flex: 1,width: 100, height: 100}}>
+          <Button 
+            onPress={ () => this.saveImage() } 
+            title="Salvar"
+          />
+          <Button 
+            onPress={ () => this.resetImage() } 
+            title="Limpar"
+          />
+        </View>
       </View>
     )
   }
