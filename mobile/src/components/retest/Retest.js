cf4ea4483d347f927f4df78fe7e7b11d02dde7d6
Alexandre Resende <aar6947@gmail.com>
2018-05-29 22:32:42 -0300

    feature: Create retest component

---
+++ mobile/src/components/retest/Retest.js
@@ -0,0 +1,38 @@
+
+import React, { Component } from 'react';
+import { CheckBox } from 'react-native';
+
+class Retest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: false,
+    };
+  }
+
+  changeCheckboxStatus = () => {
+    this.setStatus({
+      checked: !this.state.checked,
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <CheckBox
+          title='Resteste'
+          center
+          checkedIcon='dot-circle-o'
+          uncheckedIcon='dot-circle'
+          onPress={ this.changeCheckboxStatus }  
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export { Retest };
