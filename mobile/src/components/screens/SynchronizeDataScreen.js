6e302984b0696cca2e43b85629e29ba7e961a6a9
Guilherme sa <guilhermedesaster@gmail.com>
2018-06-17 20:43:40 -0300

    candidateSignature

--- mobile/src/components/screens/SynchronizeDataScreen.js
+++ mobile/src/components/screens/SynchronizeDataScreen.js
@@ -19,7 +19,7 @@ class SynchronizeDataScreen extends Component {
       // console.log(this.props.navigation.getParam('appraiserSignature',''))
       let appraiserSignature = this.props.navigation.getParam('appraiserSignature','');
       let appraiserName = this.props.navigation.getParam('name', 'Invalid Name')
-      let { name, classNumber, number, result, retest, examDate, examTime } = exam;
+      let { name, classNumber, number, result, retest, examDate, examTime, candidateSignature } = exam;
       await fetch('http://192.168.0.36:3000/exams', {
         method: 'PUT',
         headers: {
@@ -34,6 +34,7 @@ class SynchronizeDataScreen extends Component {
           number,
           result,
           retest,
+          candidateSignature,
           appraiserSignature,
           appraiserName,
         }),
